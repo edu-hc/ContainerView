aa47a022bdae3a8cf6e2f93aaf49460c75838acd
eduardo <edu.carvc@gmail.com>
2025-02-28 17:03:22 -0300

    Initial Spring project

---
+++ src/main/java/com/ftc/containerView/ContainerViewApplication.java
@@ -0,0 +1,13 @@
+package com.ftc.containerView;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ftc/containerView/ContainerViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftc.containerView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
