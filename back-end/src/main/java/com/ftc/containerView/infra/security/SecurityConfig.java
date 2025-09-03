1114bc316e0a65615d257019450746c564fb0e3f
edu-hc <edu.carvc@gmail.com>
2025-09-02 23:29:06 -0300

    feat: cors configuration for local host

--- back-end/src/main/java/com/ftc/containerView/infra/security/SecurityConfig.java
+++ back-end/src/main/java/com/ftc/containerView/infra/security/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(Arrays.asList(
+                // TODO: Add production URL
                 "http://localhost:3000"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
